ea681663c3551a835d2cad28ec3f4ee38c1792b2
daniel-black <drblack651@gmail.com>
2022-11-13 10:28:13 -0700

    Add poorly done error handling in API

--- pages/api/jobs.ts
+++ pages/api/jobs.ts
@@ -14,12 +14,23 @@ export default async function handler(
   }
   
   const request = await fetch(
-    `https://data.usajobs.gov/api/search?${searchParams.toString()}`
-    , {
+    `https://data.usajobs.gov/api/search?${searchParams.toString()}`, 
+    {
       headers: {
         'Authorization-Key': process.env.JOBS_API_KEY || ''
       }
-    });
+    }
+  );
+
+  if (request.status === 400) {
+    res.status(400).json({ msg: 'api received 400 from other api' });
+    throw new Error('error: 400');
+  } else if (request.status === 404) {
+    res.status(404).json({ msg: 'api received 404 from other api' });
+    throw new Error('error: 404');
+  }
+
+
   const requestData = await request.json();
 
   res.status(200).json({ requestData });
